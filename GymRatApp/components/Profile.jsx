323182e645e68df786c6b995e5cdd2747321fbbd
Jeremiah Howden <jeremiahhowden@gmail.com>
2020-03-07 15:40:28 -0600

    Profile fixed, content next

--- GymRatApp/components/Profile.jsx
+++ GymRatApp/components/Profile.jsx
@@ -1,57 +1,30 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import MenuDrawer from 'react-native-side-drawer'
-import { PanGestureHandler } from 'react-native-gesture-handler'
-import Swipeable from 'react-native-gesture-handler/Swipeable'
+import { DrawerLayout, PanGestureHandler } from 'react-native-gesture-handler'
  
 export default function Profile() {
-	
-	const [drawer, setDrawer] = useState(false)
 
-	function drawerContent() {
+	function profileContent() {
 		return (
-			<TouchableOpacity onPress={() => setDrawer(false)} style={styles.animatedBox}>
-				<Text>Close</Text>
-			</TouchableOpacity>
+			<View>
+				<Text>I am in the drawer!</Text>
+			</View>
 		);
 	};
  
 	return (
-		<View style={styles.container}>
-			<MenuDrawer 
-				open={drawer}
-				drawerContent={() => drawerContent()}
-				drawerPercentage={60}
-				animationTime={250}
-				overlay={true}
-				opacity={0.4}
+		<View style={{flex: 1}}>
+			<DrawerLayout
+				drawerWidth={200}
+				drawerPosition={DrawerLayout.positions.Left}
+				drawerType='front'
+				drawerBackgroundColor="#ddd"
+				renderNavigationView={profileContent}
 			>
-				<PanGestureHandler onGestureEvent={() => setDrawer(!drawer)} activeOffsetX={20}>
-					<Text>Swipe</Text>
-				</PanGestureHandler>
-			</MenuDrawer>
-		</View>
+				<View>
+					<Text> Hello, it's me </Text>
+				</View>
+			</DrawerLayout>
+    	</View>
 	);
 }
- 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-		marginTop: 30,
-		zIndex: 0
-	},
-	animatedBox: {
-		flex: 1,
-		backgroundColor: "#38C8EC",
-		padding: 10
-	},
-	body: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: '#F04812'
-	}
-})
