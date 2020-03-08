76268eb553946708d54329b62825ae0c03270cd0
Elias Moreno <elias.cmoreno17@gmail.com>
2020-03-07 23:42:18 -0600

    workout commented lol

---
+++ GymRatApp/components/Workout.jsx
@@ -0,0 +1,28 @@
+// import React, { useState, useEffect, useContext } from 'react'
+// import { StyleSheet, Text, View, TextInput, Image } from 'react-native'
+// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+// const Tab = createMaterialTopTabNavigator();
+
+
+// function MyWorkouts() {
+
+//     return ();
+
+// }
+
+// function Favorites() {
+
+// }
+
+export default function Workout(props) {
+    // return (
+    //     <Tab.Navigator>
+    //         <Tab.Screen name="MyWorkouts" component={MyWorkouts} />
+    //         <Tab.Screen name="Favorites" component={Favorites} />
+    //     </Tab.Navigator>
+    // );
+}
+
+
+
