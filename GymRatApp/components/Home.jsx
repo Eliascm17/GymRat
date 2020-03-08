e9d647ec11820e795f5002cf05b329c31d3d9ad6
Jeremiah Howden <jeremiahhowden@gmail.com>
2020-03-07 21:35:16 -0600

    library updates

--- GymRatApp/components/Home.jsx
+++ GymRatApp/components/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Text, View, Dimensions } from 'react-native'
 import { Button } from 'react-native-elements'
-import Geolocation from '@react-native-community/geolocation';
 import MapView from 'react-native-maps'
 
 import HomeMapStyle from './HomeMapStyle.json'
@@ -10,8 +9,6 @@ import userContext from '../contexts/userContext'
 function Home(props) {
 
     [gymPlaces, setGymPlaces] = useState([])
-
-    Geolocation.getCurrentPosition(info => console.log(info));
 
     return (
         <View>
