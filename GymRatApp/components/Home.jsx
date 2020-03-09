4cef009411a3cf99383c178f646d7b9fb2cb4eda
Eliascm17 <Elias.cmoreno17@gmail.com>
2020-03-09 10:53:44 -0500

    working on profile styling

--- GymRatApp/components/Home.jsx
+++ GymRatApp/components/Home.jsx
@@ -13,44 +13,46 @@ export default function Home(props) {
     const Tab = createBottomTabNavigator();
     
     return (
-      <NavigationContainer independent={true}>
-        <Tab.Navigator
-          initialRouteName="map"
-          tabBarOptions={{
-            activeTintColor: "white",
-            inactiveTintColor: "gray",
-            activeBackgroundColor: "#951BAD"
-          }}
-        >
-          <Tab.Screen
-            name="map"
-            component={Map}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesome5 name={"map"} color={color} size={size} />
-              )
+        <NavigationContainer independent={true}>
+          <Tab.Navigator
+            initialRouteName="map"
+            tabBarOptions={{
+              activeTintColor: "white",
+              inactiveTintColor: "gray",
+              activeBackgroundColor: "#951BAD",
+              marginBottom: 23
             }}
-          />
-          <Tab.Screen
-            name="workout"
-            component={Workout}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesome5 name={"dumbbell"} color={color} size={size} />
-              )
-            }}
-          />
-          <Tab.Screen
-            name="profile"
-            component={Profile}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesome5 name={"user"} color={color} size={size} />
-              )
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
+          >
+            <Tab.Screen
+              name="map"
+              component={Map}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <FontAwesome5 name={"map"} color={color} size={size} />
+                )
+              }}
+            />
+            <Tab.Screen
+              name="workout"
+              component={Workout}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <FontAwesome5 name={"dumbbell"} color={color} size={size} />
+                )
+              }}
+            />
+            <Tab.Screen
+              name="profile"
+              component={Profile}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <FontAwesome5 name={"user"} color={color} size={size} />
+                )
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      
     );
 
 }
--- GymRatApp/components/Profile.jsx
+++ GymRatApp/components/Profile.jsx
@@ -10,26 +10,60 @@ export default function Profile(props) {
 	const { user, setUser } = useContext(UserContext)
 
 		return (
-			<View style={{ flex: 1, padding: 30, alignItems: 'center', flexDirection: 'column' }}>
-				<Avatar
-					rounded
-					source={{
-						uri: "https://raw.githubusercontent.com/Eliascm17/GymRat/master/GymRatApp/assets/profile-pictures/" + (user.name.toLowerCase() || 'null') + ".jpg"
-					}}
-					size="xlarge"
-				/>
-				<Text style={{ paddingTop: 20, fontFamily: 'System', fontSize: 28 }} onLongPress={() => setUser({ name: "", bio: "", points: 0 })}>
-					{user.name || "Not logged in..."}
-				</Text>
-				<Text style={{ paddingTop: 20, flexGrow: 1, fontFamily: 'System', fontSize: 20 }}>
-					{user.bio || "Certified Gym Rat"}
-				</Text>
-				<View style={{ paddingVertical: 20, flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-					<Text style={{ fontFamily: 'System', fontSize: 28, color: '#951BAD' }}>
-						{user.points.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-					</Text>
-					<Icon name='bolt' type='font-awesome' color='#951BAD' iconStyle={{ paddingLeft: 3 }} />
-				</View>
-			</View>
-		);
+      <View
+        style={{
+          flex: 1,
+          padding: 30,
+          alignItems: "center",
+          flexDirection: "column"
+        }}
+      >
+        <View style={{ alignItems: 'center', paddingTop: 50}}>
+          <Avatar
+            rounded
+            source={{
+              uri:
+                "https://raw.githubusercontent.com/Eliascm17/GymRat/master/GymRatApp/assets/profile-pictures/" +
+                (user.name.toLowerCase() || "null") +
+                ".jpg"
+            }}
+            size="xlarge"
+          />
+          <Text
+            style={{ paddingTop: 20, fontFamily: "System", fontSize: 28 }}
+            onLongPress={() => setUser({ name: "", bio: "", points: 0 })}
+          >
+            {user.name || "Not logged in..."}
+          </Text>
+          <Text
+            style={{
+              paddingTop: 20,
+              flexGrow: 1,
+              fontFamily: "System",
+              fontSize: 20
+            }}
+          >
+            {user.bio || "Certified Gym Rat"}
+          </Text>
+        </View>
+        <View
+          style={{
+			flexDirection: "row",
+			alignItems: "center"
+          }}
+        >
+          <Text
+            style={{ fontFamily: "System", fontSize: 28, color: "#951BAD" }}
+          >
+            {user.points.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </Text>
+          <Icon
+            name="bolt"
+            type="font-awesome"
+            color="#951BAD"
+            iconStyle={{ paddingLeft: 3 }}
+          />
+        </View>
+      </View>
+    );
 	};
